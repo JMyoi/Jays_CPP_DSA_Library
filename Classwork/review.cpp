e59e621cb3385a7b1c63ba9d2841b08269e5f771
Jay Chen <jaychenjjc@gmail.com>
2026-09-15 10:51:42 -0400

    for each

--- Classwork/review.cpp
+++ Classwork/review.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-
-    cout << "Hello world"<<endl;
+    int nums[10] = {2,2,2,2};
+    for(int num: nums) {
+        cout << num << endl;
+        
+    }   
     return 0;
 }
